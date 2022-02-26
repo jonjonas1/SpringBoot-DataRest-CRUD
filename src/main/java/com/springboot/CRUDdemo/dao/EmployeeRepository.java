c1c4ebce730fef82543ea08a0b58ec2f2a60dc98
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-02-26 12:30:02 -0600

    added pagination and properties

--- src/main/java/com/springboot/CRUDdemo/dao/EmployeeRepository.java
+++ src/main/java/com/springboot/CRUDdemo/dao/EmployeeRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.CRUDdemo.entity.Employee;
 
+//@RepositoryRestResource(path="members") //8080/magic-api/members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// that's it ... no need to write any code LOL!
